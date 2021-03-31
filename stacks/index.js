8292e564e19cc20a7b9c549397cd7daca5f5081f
Raul Bunicenha <raulbunicenha@gmail.com>
2021-03-31 10:55:28 -0300

    Change the array standard behaviour and add peek function

--- stacks/index.js
+++ stacks/index.js
@@ -8,3 +8,18 @@ import PalindromeWord from './PalindromeWord.js'
 const word = 'ana'
 const isPalindrome = new PalindromeWord(word).build()
 console.log(isPalindrome ? `${word} is a palindrome` : `${word} is not a palindrome`)
+
+/**
+ * Change the array standard behaviour and add peek function
+ */
+{
+    Array.prototype.peek = function () {
+        if (this.length == 0) return null
+        return this[this.length - 1]
+    }
+    
+    let stack = []
+    stack.push('first Item')
+    stack.push('second Item')
+    console.log(`The peek return item is:`, stack.peek())
+}
